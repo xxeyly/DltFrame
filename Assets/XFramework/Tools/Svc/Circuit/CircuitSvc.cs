75913fbb8fa13ffbadd94818af155df6c88234b2
xxslit <sltkit@163.com>
2021-12-09 09:48:14 +0800

    流程服务更改

--- Assets/XFramework/Tools/Svc/Circuit/CircuitSvc.cs
+++ Assets/XFramework/Tools/Svc/Circuit/CircuitSvc.cs
@@ -11,11 +11,12 @@ namespace XFramework
         public static CircuitSvc Instance;
 
         [BoxGroup] [TableList(AlwaysExpanded = true, DrawScrollView = false)] [LabelText("场景缓存流程数据")]
-        public CircuitTempData circuitTempData;
+        public CircuitTempData circuitTempData = new CircuitTempData();
 
+        [LabelText("上一个流程")] private Type _lastCircuitBaseData;
         [LabelText("场景流程")] public List<CircuitBaseData> sceneCircuitBaseData;
         private Dictionary<Type, CircuitBaseData> _allCircuitBaseDataDic;
-        private Type _oldCircuitBaseData;
+        [LabelText("流程执行中")] private bool _inExecution;
 
         public override void StartSvc()
         {
@@ -52,44 +53,35 @@ namespace XFramework
             EntitySvc.Instance.onHideEntity -= OnHideEntity;
         }
 
+        /// <summary>
+        /// 执行流程
+        /// </summary>
+        /// <param name="circuitType"></param>
         public void StartCircuit(Type circuitType)
         {
-            ClearTempCircuit(true);
-            if (_oldCircuitBaseData != null)
-            {
-                _allCircuitBaseDataDic[_oldCircuitBaseData].EndCircuit();
-            }
-
-            _oldCircuitBaseData = circuitType;
-
+            EndCircuit();
+            _inExecution = true;
+            _lastCircuitBaseData = circuitType;
             _allCircuitBaseDataDic[circuitType].StartCircuit();
         }
 
-        public void StartCircuit(Type circuitType, bool clearView)
+        /// <summary>
+        /// 停止流程
+        /// </summary>
+        public void EndCircuit()
         {
-            ClearTempCircuit(clearView);
-            if (_oldCircuitBaseData != null)
+            if (_lastCircuitBaseData != null)
             {
-                _allCircuitBaseDataDic[_oldCircuitBaseData].EndCircuit();
+                _allCircuitBaseDataDic[_lastCircuitBaseData].EndCircuit();
             }
 
-            _oldCircuitBaseData = circuitType;
-            _allCircuitBaseDataDic[circuitType].StartCircuit();
-        }
-
-
-        private void ClearTempCircuit(bool clearView)
-        {
-            if (clearView)
-            {
-                Type[] viewType = DataSvc.DataValueClone(circuitTempData.activityViewType).ToArray();
-                ViewSvc.Instance.HideView(viewType);
-            }
-
+            //隐藏视图
+            Type[] viewType = DataSvc.DataValueClone(circuitTempData.activityViewType).ToArray();
+            ViewSvc.Instance.HideView(viewType);
+            //结束时间监听
             List<int> timeTask = DataSvc.DataValueClone(circuitTempData.timeTask);
             TimeSvc.Instance.DeleteTimeTask(timeTask);
             circuitTempData.timeTask.Clear();
-
             List<int> switchTimeTask = DataSvc.DataValueClone(circuitTempData.switchTask);
             for (int i = 0; i < switchTimeTask.Count; i++)
             {
@@ -97,29 +89,24 @@ namespace XFramework
             }
 
             circuitTempData.switchTask.Clear();
-
+            //隐藏实体
             EntitySvc.Instance.DisplayEntityByEntityName(false, DataSvc.DataValueClone(circuitTempData.entity).ToArray());
             circuitTempData.entity.Clear();
+            //结束流程控制
+            _inExecution = false;
         }
 
         /// <summary>
-        /// 注册流程
+        /// 隐藏实体
         /// </summary>
-        /// <param name="circuitBaseData"></param>
-        public void RegisterCircuit(CircuitBaseData circuitBaseData)
-        {
-            if (_allCircuitBaseDataDic.ContainsKey(circuitBaseData.GetType()))
-            {
-                Debug.LogError(circuitBaseData.GetType() + "已经注册过");
-            }
-            else
-            {
-                _allCircuitBaseDataDic.Add(circuitBaseData.GetType(), circuitBaseData);
-            }
-        }
-
+        /// <param name="entityName"></param>
         private void OnHideEntity(string entityName)
         {
+            if (!_inExecution)
+            {
+                return;
+            }
+
             for (int i = 0; i < circuitTempData.entity.Count; i++)
             {
                 if (circuitTempData.entity[i] == entityName)
@@ -130,13 +117,31 @@ namespace XFramework
             }
         }
 
+        /// <summary>
+        /// 显示实体
+        /// </summary>
+        /// <param name="entityName"></param>
         private void OnShowEntity(string entityName)
         {
+            if (!_inExecution)
+            {
+                return;
+            }
+
             circuitTempData.entity.Add(entityName);
         }
 
+        /// <summary>
+        /// 隐藏实体
+        /// </summary>
+        /// <param name="type"></param>
         private void OnHideShow(Type type)
         {
+            if (!_inExecution)
+            {
+                return;
+            }
+
             for (int i = 0; i < circuitTempData.activityViewType.Count; i++)
             {
                 if (circuitTempData.activityViewType[i] == type)
@@ -147,18 +152,46 @@ namespace XFramework
             }
         }
 
+        /// <summary>
+        /// 显示视图
+        /// </summary>
+        /// <param name="type"></param>
         private void OnViewShow(Type type)
         {
+            if (!_inExecution)
+            {
+                return;
+            }
+
             circuitTempData.activityViewType.Add(type);
         }
 
+        /// <summary>
+        /// 增加监听
+        /// </summary>
+        /// <param name="tid"></param>
+        /// <param name="timeName"></param>
         private void OnTimeAddTimeTask(int tid, string timeName)
         {
+            if (!_inExecution)
+            {
+                return;
+            }
+
             circuitTempData.timeTask.Add(tid);
         }
 
+        /// <summary>
+        /// 删除监听
+        /// </summary>
+        /// <param name="tid"></param>
         private void OnTimeDeleteTimeTask(int tid)
         {
+            if (!_inExecution)
+            {
+                return;
+            }
+
             for (int i = 0; i < circuitTempData.timeTask.Count; i++)
             {
                 if (circuitTempData.timeTask[i] == tid)
@@ -169,13 +202,32 @@ namespace XFramework
             }
         }
 
+        /// <summary>
+        /// 增加监听   
+        /// </summary>
+        /// <param name="tid"></param>
+        /// <param name="timeName"></param>
         private void OnTimeAddSwitchTimeTask(int tid, string timeName)
         {
+            if (!_inExecution)
+            {
+                return;
+            }
+
             circuitTempData.switchTask.Add(tid);
         }
 
+        /// <summary>
+        /// 删除监听
+        /// </summary>
+        /// <param name="tid"></param>
         private void OnTimeDeleteSwitchTimeTask(int tid)
         {
+            if (!_inExecution)
+            {
+                return;
+            }
+
             for (int i = 0; i < circuitTempData.switchTask.Count; i++)
             {
                 if (circuitTempData.switchTask[i] == tid)
---
+++ Assets/XFramework/View/BaseWindow/BaseWindowCircuit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace XFramework
+{
+    public partial class BaseWindow
+    {
+        /// <summary>
+        /// 开始流程
+        /// </summary>
+        /// <param name="circuitBaseData"></param>
+        public void StartCircuit(Type circuitBaseData)
+        {
+            CircuitSvc.Instance.StartCircuit(circuitBaseData);
+        }
+
+        /// <summary>
+        /// 结束流程
+        /// </summary>
+        public void EndCircuit()
+        {
+            CircuitSvc.Instance.EndCircuit();
+        }
+    }
+}
