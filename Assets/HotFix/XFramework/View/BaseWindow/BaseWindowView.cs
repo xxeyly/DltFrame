26e8695060f9d4e01a6f5a67108cd59b5f9bcff8
xxeyly <xxeyly@163.com>
2022-07-12 08:43:06 +0800

    框架编辑器和运行代码分离

---
+++ Assets/HotFix/XFramework/View/BaseWindow/BaseWindowView.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace XFramework
+{
+    partial class BaseWindow
+    {
+        /// <summary>
+        /// 获得某个视图的显示状态
+        /// </summary>
+        /// <param name="view"></param>
+        /// <returns></returns>
+        protected bool GetViewState(Type view)
+        {
+            return ViewComponent.Instance.GetViewState(view);
+        }
+
+
+        /// <summary>
+        /// 获得当前活动的视图数量
+        /// </summary>
+        /// <returns></returns>
+        protected int GetCurrentActiveViewCount()
+        {
+            return ViewComponent.Instance.GetCurrentActiveViewCount();
+        }
+
+
+        #region 显示视图
+
+        protected void ShowThisView()
+        {
+            ViewComponent.Instance.ShowView(viewType);
+        }
+
+        /// <summary>
+        /// 显示单一视图类型
+        /// </summary>
+        /// <param name="type"></param>
+        protected void ShowView(Type type)
+        {
+            ViewComponent.Instance.ShowView(type);
+        }
+
+        /// <summary>
+        /// 显示一些视图
+        /// </summary>
+        /// <param name="types"></param>
+        protected void ShowView(params Type[] types)
+        {
+            ViewComponent.Instance.ShowView(types);
+        }
+
+        /// <summary>
+        /// 等待一段时间后,显示视图
+        /// </summary>
+        /// <param name="type">视图类型</param>
+        /// <param name="time">切换所需时间</param>
+        protected void ShowView(Type type, float time)
+        {
+            ViewComponent.Instance.ShowView(type, time);
+        }
+
+        /// <summary>
+        /// 等待一段时间后,显示视图
+        /// </summary>
+        /// <param name="typeList"></param>
+        /// <param name="time">切换所需时间</param>
+        protected void ShowView(float time, params Type[] typeList)
+        {
+            ViewComponent.Instance.ShowView(time, typeList);
+        }
+
+        #endregion
+
+        #region 隐藏视图
+
+        /// <summary>
+        /// 显示单一视图类型
+        /// </summary>
+        /// <param name="type"></param>
+        protected void HideView(Type type)
+        {
+            ViewComponent.Instance.HideView(type);
+        }
+
+        /// <summary>
+        /// 隐藏一些视图
+        /// </summary>
+        /// <param name="types"></param>
+        protected void HideView(params Type[] types)
+        {
+            ViewComponent.Instance.HideView(types);
+        }
+
+        /// <summary>
+        /// 等待一段时间后,隐藏视图
+        /// </summary>
+        /// <param name="type">视图类型</param>
+        /// <param name="time">切换所需时间</param>
+        protected void HideView(Type type, float time)
+        {
+            ViewComponent.Instance.HideView(type, time);
+        }
+
+        /// <summary>
+        /// 等待一段时间后,隐藏视图
+        /// </summary>
+        /// <param name="types"></param>
+        /// <param name="time">切换所需时间</param>
+        protected void HideView(float time, params Type[] types)
+        {
+            ViewComponent.Instance.HideView(time, types);
+        }
+
+        /// <summary>
+        /// 隐藏视图
+        /// </summary>
+        protected void HideAllView()
+        {
+            ViewComponent.Instance.HideAllView();
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/XFrameworkEditor/Editor/View/EditorPanel/FrameMenu.cs
@@ -0,0 +1,133 @@
+﻿#if UNITY_EDITOR
+
+using Sirenix.OdinInspector.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace XFramework
+{
+    public class FrameMenu : OdinMenuEditorWindow
+    {
+        private FrameMenu()
+        {
+        }
+
+
+        [MenuItem("Xframe/框架界面")]
+        private static void OpenWindow()
+        {
+            GetWindow<FrameMenu>().Show();
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            _audioComponentEditor.OnDisable();
+            customBuild.OnDisable();
+            gameRootEditor.OnDisable();
+            generateBaseWindowEditor.OnDisable();
+            sceneLoad.OnDisable();
+            frameComponent.OnDisable();
+            AssetDatabase.SaveAssets();
+        }
+
+        [MenuItem("Xframe/监听生成 &l")]
+        private static void OnListenerGenerate()
+        {
+            ListenerComponentGenerateData listenerComponentGenerateData =
+                DataComponent.GetObjectsInScene<ListenerComponentGenerateData>();
+            if (listenerComponentGenerateData != null)
+            {
+                listenerComponentGenerateData.OnGenerate();
+                Debug.Log("监听生成完毕");
+            }
+            else
+            {
+                Debug.LogWarning(" ListenerComponent未添加");
+            }
+        }
+
+        //打包
+        private static CustomBuild customBuild = new CustomBuild();
+
+        //音频组件
+        private AudioComponentEditor _audioComponentEditor = new AudioComponentEditor();
+
+        //框架配置
+        private static GameRootEditor gameRootEditor;
+        private GenerateBaseWindowEditor generateBaseWindowEditor = new GenerateBaseWindowEditor();
+        private static SceneLoad sceneLoad = new SceneLoad();
+
+        //可持久化
+        RuntimeDataComponentEditor _runtimeDataComponentEditor = new RuntimeDataComponentEditor();
+
+        //资源组件
+        ResComponentEditor _resComponentEditor = new ResComponentEditor();
+
+        //下载组件
+        DownComponentEditor _downComponentEditor = new DownComponentEditor();
+
+        //监听组件
+        ListenerComponentEditor _listenerComponentEditor = new ListenerComponentEditor();
+
+        //场景组件
+        SceneLoadComponentEditor _sceneLoadComponentEditor = new SceneLoadComponentEditor();
+
+        //计时器组件
+        TimeComponentEditor _timeComponentEditor = new TimeComponentEditor();
+
+        //视图组件
+        ViewComponentEditor _viewComponentEditor = new ViewComponentEditor();
+
+        //实体组件
+        EntityComponentEditor _entityComponentEditor = new EntityComponentEditor();
+
+        //流程组件
+        CircuitComponentEditor _circuitComponentEditor = new CircuitComponentEditor();
+
+        //鼠标组件
+        MouseComponentEditor _mouseComponentEditor = new MouseComponentEditor();
+
+        //资源统一化
+        ResourceUnification resourceUnification = new ResourceUnification();
+
+        //动画工具
+        AnimTools animTools = new AnimTools();
+        FrameComponent frameComponent = new FrameComponent();
+
+        //生成配置
+        protected override OdinMenuTree BuildMenuTree()
+        {
+            var tree = new OdinMenuTree();
+            tree.Selection.SupportsMultiSelect = false;
+            //框架配置
+            gameRootEditor = new GameRootEditor(_runtimeDataComponentEditor, _resComponentEditor, _downComponentEditor,
+                _audioComponentEditor, _listenerComponentEditor, _sceneLoadComponentEditor, _timeComponentEditor,
+                _entityComponentEditor, _viewComponentEditor,
+                _circuitComponentEditor, _mouseComponentEditor);
+            sceneLoad.OnInit();
+            customBuild.OnInit();
+            gameRootEditor.OnInit();
+            _audioComponentEditor.OnInit();
+            generateBaseWindowEditor.OnInit();
+            resourceUnification.OnInit();
+            frameComponent.OnInit();
+            customBuild.AfferentSceneLoad(sceneLoad);
+            tree.Add("打包工具", customBuild);
+            tree.Add("场景编辑", sceneLoad);
+            tree.Add("框架配置", gameRootEditor);
+            tree.Add("音频配置", _audioComponentEditor);
+            tree.Add("生成配置", generateBaseWindowEditor);
+            tree.Add("资源统一化", resourceUnification);
+            tree.Add("动画工具", animTools);
+            tree.Add("框架组件", frameComponent);
+            return tree;
+        }
+
+
+        private void OnDisable()
+        {
+        }
+    }
+}
+#endif
