773c86b70118c066bb70685013e9b6c7789e8cfe
xxeyly <xxeyly@163.com>
2025-01-21 11:21:32 +0800

    V1.1.7
       -代码生成时机改为编译完后生成
      - 当代码编辑后如果有错误,将会自动停止生成

--- Assets/DltFramework/Editor/Generate/AutoListenerGenerate.cs
+++ Assets/DltFramework/Editor/Generate/AutoListenerGenerate.cs
@@ -1,54 +1,73 @@
-using System;
 using System.IO;
-using DltFramework;
 using UnityEditor;
+using UnityEditor.Compilation;
 using UnityEngine;
 
 
 [InitializeOnLoad]
 public class AutoListenerGenerate
 {
-    private static long oldTime;
-    private static TimeSpan st1;
     private static AutoGenerateListenerConfig autoGenerateListenerConfig;
+    public static bool isAssemblyError;
 
     static AutoListenerGenerate()
     {
-        EditorApplication.update += Update;
+        // 监听编译完成的事件
+        CompilationPipeline.assemblyCompilationFinished += OnAssemblyCompilationFinished;
+        AssemblyReloadEvents.afterAssemblyReload += OnAfterAssemblyReload;
     }
 
-    static void Update()
+    private static void OnAfterAssemblyReload()
     {
-        st1 = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0);
-
-        if (Convert.ToInt64(st1.TotalSeconds) - oldTime >= 1)
+        if (!isAssemblyError)
         {
-            if (!Directory.Exists("Assets/Config/"))
-            {
-                Directory.CreateDirectory("Assets/Config/");
-                AssetDatabase.Refresh();
-            }
-
-            if (!File.Exists("Assets/Config/" + "AutoGenerateListenerConfig.asset"))
-            {
-                AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<AutoGenerateListenerConfig>(), "Assets/Config/" + "AutoGenerateListenerConfig.asset");
-            }
-
-            if (autoGenerateListenerConfig == null)
-            {
-                autoGenerateListenerConfig = AssetDatabase.LoadAssetAtPath<AutoGenerateListenerConfig>("Assets/Config/" + "AutoGenerateListenerConfig.asset");
-                autoGenerateListenerConfig.isAuto = true;
-                EditorUtility.SetDirty(autoGenerateListenerConfig);
-            }
-
-
-            if (!autoGenerateListenerConfig.isAuto)
-            {
-                return;
-            }
-
-            oldTime = Convert.ToInt64(st1.TotalSeconds);
-            GenerateListenerComponent.GenerateListener();
+            GenerateCode();
         }
+    }
+
+    private static async void OnAssemblyCompilationFinished(string assemblyPath, CompilerMessage[] compilerMessages)
+    {
+        // 如果有编译错误，停止代码生成
+        bool hasError = false;
+        foreach (var message in compilerMessages)
+        {
+            if (message.type == CompilerMessageType.Error)
+            {
+                hasError = true;
+                break;
+            }
+        }
+
+        isAssemblyError = hasError;
+    }
+
+
+    static void GenerateCode()
+    {
+        if (!Directory.Exists("Assets/Config/"))
+        {
+            Directory.CreateDirectory("Assets/Config/");
+            AssetDatabase.Refresh();
+        }
+
+        if (!File.Exists("Assets/Config/" + "AutoGenerateListenerConfig.asset"))
+        {
+            AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<AutoGenerateListenerConfig>(), "Assets/Config/" + "AutoGenerateListenerConfig.asset");
+        }
+
+        if (autoGenerateListenerConfig == null)
+        {
+            autoGenerateListenerConfig = AssetDatabase.LoadAssetAtPath<AutoGenerateListenerConfig>("Assets/Config/" + "AutoGenerateListenerConfig.asset");
+            autoGenerateListenerConfig.isAuto = true;
+            EditorUtility.SetDirty(autoGenerateListenerConfig);
+        }
+
+
+        if (!autoGenerateListenerConfig.isAuto)
+        {
+            return;
+        }
+
+        GenerateListenerComponent.GenerateListener();
     }
 }
